736e882db7e7a05aa1de4bb6ff473ea1c267dd89
arkan <218509278+sarapil@users.noreply.github.com>
2026-04-11 07:35:04 +0000

    fix(base_base): update hooks.py asset references

--- base_base/hooks.py
+++ base_base/hooks.py
@@ -29,8 +29,8 @@ add_to_apps_screen = [
 # ------------------
 
 # include js, css files in header of desk.html
-app_include_css = ["/assets/base_base/css/base_base_combined.css"]
-app_include_js = ["/assets/base_base/js/base_base_combined.js"]
+# MEGA: app_include_css = ["/assets/base_base/css/base_base_combined.css"]
+# MEGA: app_include_js = ["/assets/base_base/js/base_base_combined.js"]
 
 # include js, css files in header of web template
 # web_include_css = "/assets/base_base/css/base_base.css"
